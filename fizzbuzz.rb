eb5512fb7e6e86e1dfccb1b70e3454c4e9b1a2b8
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2021-12-08 16:50:53 -0500

    Final commit :sunglasses:

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,11 @@
-ea2d254e-5870-11ec-be62-83c8df54a1ef
+(1..100).each do |n|
+  puts if (n % 15).zero?
+    "FizzBuzz"
+  elsif (n % 5).zero?
+    "Buzz"
+  elsif (n % 3).zero?
+    "Fizz"
+  else
+    n
+  end
+end
